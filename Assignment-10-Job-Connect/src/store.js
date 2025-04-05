ea2fea55c83e79a38c4052cb628d2f3e8759652b
Vikas Meneni <meneni.v@northeastern.edu>
2025-04-05 16:15:43 -0400

    Delete Assignment-10-Job-Connect directory

--- Assignment-10-Job-Connect/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import jobReducer from './features/jobSlice';
-
-export const store = configureStore({
-    reducer: {
-        job: jobReducer,
-    },
-});
